b370b7621e887e8faeff07f2997f3a995b8504ee
nevakrien <nevo.krien@gmail.com>
2025-03-19 11:28:18 +0200

    added perms thingy

--- second.scm
+++ second.scm
@@ -1,13 +1,15 @@
 #lang scheme
 
-;needs work
-
+;finds the semetric diffrence of 2 lists
+;this function has stable indexing
 (define (set-dif a b)
+  ;checks if the element x is in the list s
   (define (is-in x s)
     (if (null? s) #f
     (if (equal? (car s) x) #t (is-in x (cdr s)))
     ))
 
+  ;gathers all elementts in "in" that are not in "ref"
   (define (get-missing in ref)
     (foldr (λ (x acc) (if (is-in x ref)
           acc (cons x acc )))
---
+++ third.scm
@@ -0,0 +1,30 @@
+#lang scheme
+
+
+;slowly drains elements from right creating pairs left+right1 x right2
+(define (elem-permute left x right)
+  (define new (append left (cons x right)))
+
+  (if (null? right)  (list new)
+       (cons  new
+             (elem-permute
+                 (append left (list(car right)))
+                 x
+                 (cdr right)
+
+              )))
+   )
+
+(define  (my_permutations l)
+  (if (null? (cdr l)) (list l)
+  (foldl (λ (perm acc)
+           (append acc (elem-permute null (car l) perm))
+           )
+         null
+         (my_permutations (cdr l))
+   )
+))
+
+;(elem-permute null 'x '(1 2 3 4) )
+
+(my_permutations '(1 2 3))
