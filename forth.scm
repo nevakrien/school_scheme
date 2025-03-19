63c99ed88ac258059b0b3bf2cda7dda3c3942225
nevakrien <nevo.krien@gmail.com>
2025-03-19 12:22:46 +0200

    done? need unit tests probably?

--- forth.scm
+++ forth.scm
@@ -2,6 +2,8 @@
 
 (define ____ null)
 
+;removes the even numbers (recursivly) and counts how many were removed
+;input (x 0) -> (y count)
 (define foo
     (lambda (ls s)
     (cond
@@ -9,9 +11,13 @@
        [(pair? (car ls))
           (let ((p (foo (car ls) s)))
             (let ((p1 (foo (cdr ls) (cdr p))))
-              `(,(cons ____ ____) . ,(cdr p1))))]
+              `(,(cons (car p) (car p1)) . ,(cdr p1))))]
        [(or (null? (car ls)) (odd? (car ls)))
          (let ((p (foo (cdr ls) s)))
-           `(,(cons ____ ____) . ,(cdr p)))]
+           `(,(cons (car ls) (car p)) . ,(cdr p)))]
        [else (let ((p (foo (cdr ls) s)))
-               `(,(car p) . ,(add1 (cdr p))))])))
+         `(,(car p) . ,(add1 (cdr p))))])))
+
+
+
+(foo '(3 1 2 4 2 (1 2)) 0)
